99f1becd7c1b009f0f07e58adab34d80a05d303a
honore123 <imanishimwehono@gmail.com>
2025-12-06 17:54:13 +0200

    Change string length to 191 for production db

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\Schema;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     {
         // Register policies - Laravel will auto-discover from App\Policies namespace
         // based on convention: ModelPolicy for Model
-        
+        Schema::defaultStringLength(191);
         // Implicitly grant "admin" role all permissions
         // This works in the app by using gate interception
         Gate::before(function ($user, $ability) {
